6b87272cfdee0dc49a9cf945ccdbf7fd55b76b28
cdrn <chrisdoran@protonmail.com>
2023-12-04 18:16:19 +1100

    chore: scaffold the challenges

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod day1;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    match args.get(1).map(|s| s.as_str()) {
+        Some("day1") => day1::run(),
+        // Some("day2") => day2::run(),
+        // ... handle other days
+        _ => eprintln!("Specify the day to run"),
+    }
 }
